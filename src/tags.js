b6a2102e8d4ba7197e689eea51dea3793c7384f6
David Asaolu <67129211+dha-stix@users.noreply.github.com>
2023-08-13 15:13:18 +0100

    Update tags.js

--- src/tags.js
+++ src/tags.js
@@ -9,10 +9,11 @@ export const suggestions = [
 		text: type.text,
 	};
 });
-export const delimiters = [KeyCodes.comma, KeyCodes.enter];
-export const KeyCodes = {
+const KeyCodes = {
 	comma: 188,
 	enter: 13,
 };
+export const delimiters = [KeyCodes.comma, KeyCodes.enter];
+
 
 export const generateID = () => Math.random().toString(36).substring(2, 10);
